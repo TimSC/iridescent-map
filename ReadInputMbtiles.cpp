3376094d6f9c90bcb03c36e2400427c2ba917630
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2016-10-29 23:35:12 +0100

    Compile mbtile decoder into project

--- ReadInputMbtiles.cpp
+++ ReadInputMbtiles.cpp
@@ -3,8 +3,50 @@
 #include <iostream>
 #include <sstream>
 #include "FeatureStore.h"
+#include "mbtiles-cpp/VectorTile.h"
+#include "cppGzip/DecodeGzip.h"
 
 using namespace std;
+
+int ReadFileContents(const char *filename, int binaryMode, std::string &contentOut)
+{
+	contentOut = "";
+	std::ios_base::openmode mode = (std::ios_base::openmode)0;
+	if(binaryMode)
+		mode ^= std::ios::binary;
+	std::ifstream file(filename, mode);
+	if(!file) return 0;
+
+	file.seekg(0,std::ios::end);
+	std::streampos length = file.tellg();
+	file.seekg(0,std::ios::beg);
+
+	std::vector<char> buffer(length);
+	file.read(&buffer[0],length);
+
+	contentOut.assign(&buffer[0], length);
+	return 1;
+}
+
+class ExampleDataStore : public DecodeVectorTileResults
+{
+public:
+	ExampleDataStore() : DecodeVectorTileResults()
+	{
+		cout << "Create custom data store..." << endl;
+	}
+
+	void Feature(int typeEnum, bool hasId, unsigned long long id, 
+		const std::map<std::string, std::string> &tagMap,
+		std::vector<Point2D> &pointsOut, 
+		std::vector<std::vector<Point2D> > &linesOut,
+		std::vector<Polygon2D> &polygonsOut)
+	{
+		//In real use, delete this function call and add your own functionality.
+		DecodeVectorTileResults::Feature(typeEnum, hasId, id, 
+			tagMap, pointsOut, linesOut, polygonsOut);
+	}
+};
 
 ///ReadInput reads a file, preprocesses it and outputs a FeatureStore representation of the data.
 void ReadInputMbtiles(int zoom, const char *basePath, int xtile, int ytile, FeatureStore &featureStore)
@@ -17,14 +59,25 @@ void ReadInputMbtiles(int zoom, const char *basePath, int xtile, int ytile, Feat
 	string fina = finaStr.str();
 	cout << fina << endl;
 	std::filebuf fi;
-	std::filebuf* ret = fi.open(fina.c_str(), std::ios::in);
+	std::filebuf* ret = fi.open(fina.c_str(), std::ios::in | std::ios::binary);
 	if(ret == NULL)
 		throw std::runtime_error("Error opening input file");
 
+	//Ungzip the data
+	DecodeGzip dec(fi);
+	string tileData;
 
-	
+	char tmp[1024];
+	while(dec.in_avail())
+	{
+		streamsize bytes = dec.sgetn(tmp, 1024);
+		tileData.append(tmp, bytes);
+	}
 
-
+	//Decode vector data
+	class ExampleDataStore results;
+	class DecodeVectorTile vectorDec(results);
+	vectorDec.DecodeTileData(tileData, zoom, xtile, ytile);
 
 	featureStore.Clear();
 }
