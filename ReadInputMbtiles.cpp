851f57006207193ff2bcf8027cf0188a317690bd
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2016-10-31 18:03:45 +0000

    Convert mbtile data to feature store

--- ReadInputMbtiles.cpp
+++ ReadInputMbtiles.cpp
@@ -2,8 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include "FeatureStore.h"
-#include "mbtiles-cpp/VectorTile.h"
+#include "ReadInputMbtiles.h"
 #include "cppGzip/DecodeGzip.h"
 
 using namespace std;
@@ -28,26 +27,90 @@ int ReadFileContents(const char *filename, int binaryMode, std::string &contentO
 	return 1;
 }
 
-class FeatureDataStore : public DecodeVectorTileResults
+FeatureDataStore::FeatureDataStore(FeatureStore &featureStoreIn) : DecodeVectorTileResults(), featureStore(featureStoreIn)
 {
-public:
-	FeatureStore &featureStore;
 
-	FeatureDataStore(FeatureStore &featureStoreIn) : DecodeVectorTileResults(), featureStore(featureStoreIn)
-	{
+}
 
-	}
+void FeatureDataStore::NumLayers(int numLayers)
+{
 
-	void Feature(int typeEnum, bool hasId, unsigned long long id, 
-		const std::map<std::string, std::string> &tagMap,
-		std::vector<Point2D> &pointsOut, 
-		std::vector<std::vector<Point2D> > &linesOut,
-		std::vector<Polygon2D> &polygonsOut)
-	{
-		//DecodeVectorTileResults::Feature(typeEnum, hasId, id, 
-		//	tagMap, pointsOut, linesOut, polygonsOut);
-	}
-};
+}
+
+void FeatureDataStore::LayerStart(const char *name, int version)
+{
+
+}
+
+void FeatureDataStore::LayerEnd()
+{
+
+}
+
+void FeatureDataStore::Feature(int typeEnum, bool hasId, unsigned long long id, 
+	const std::map<std::string, std::string> &tagMap,
+	std::vector<Point2D> &points, 
+	std::vector<std::vector<Point2D> > &lines,
+	std::vector<Polygon2D> &polygons)
+{
+	string typeStr = FeatureTypeToStr(typeEnum);
+
+	if(typeStr == "Point")
+		for(size_t i =0; i < points.size(); i++)
+		{
+			FeaturePoi poi(tagMap, 0, points[i].first, points[i].second);
+			this->featureStore.pois.push_back(poi);
+		}
+	if(typeStr == "LineString")
+		for(size_t i =0; i < lines.size(); i++)
+		{
+			IdLatLonList line;
+			vector<Point2D> &linePts = lines[i];
+			for(size_t j =0; j < linePts.size(); j++)
+			{
+				IdLatLon pt(linePts[j].first, linePts[j].second, 0);
+				line.push_back(pt);
+			}
+
+			FeatureLine lineFeat(tagMap, line);
+			this->featureStore.lines.push_back(lineFeat);
+		}
+	if(typeStr == "Polygon")
+		for(size_t i =0; i < polygons.size(); i++)
+		{
+			Polygon2D &polygon = polygons[i];
+			std::vector<IdLatLonList> outerShapes;
+			std::vector<IdLatLonList> innerShapes;
+
+			IdLatLonList outerLine;
+			LineLoop2D &linePts = polygon.first; //Outer shape
+			for(size_t j =0; j < linePts.size(); j++)
+			{
+				IdLatLon pt(linePts[j].first, linePts[j].second, 0);
+				outerLine.push_back(pt);
+			}
+			outerShapes.push_back(outerLine);
+
+			if(polygon.second.size() > 0)
+			{
+				//Inner shape
+				for(size_t k =0; k < polygon.second.size(); k++)
+				{
+					IdLatLonList innerLine;
+					LineLoop2D &linePts2 = polygon.second[k];
+					for(size_t j =0; j < linePts2.size(); j++)
+					{
+						IdLatLon pt(linePts2[j].first, linePts2[j].second, 0);
+						innerLine.push_back(pt);
+					}
+					innerShapes.push_back(innerLine);
+				}
+			}
+
+			FeatureArea area(tagMap, outerShapes, innerShapes);
+			this->featureStore.areas.push_back(area);
+		}
+}
 
 ///ReadInput reads a file, preprocesses it and outputs a FeatureStore representation of the data.
 void ReadInputMbtiles(int zoom, const char *basePath, int xtile, int ytile, FeatureStore &featureStore)
--- ReadInputMbtiles.h
+++ ReadInputMbtiles.h
@@ -1,9 +1,32 @@
 #ifndef _READ_INPUT_MBTILES_H
 #define _READ_INPUT_MBTILES_H
 
+#include <vector>
+#include <map>
 #include "Regrouper.h"
+#include "mbtiles-cpp/VectorTile.h"
+#include "FeatureStore.h"
 
 void ReadInputMbtiles(int zoom, const char *basePath, int xtile, int ytile, FeatureStore &featureStore);
+int ReadFileContents(const char *filename, int binaryMode, std::string &contentOut);
+
+class FeatureDataStore : public DecodeVectorTileResults
+{
+public:
+	FeatureStore &featureStore;
+
+	FeatureDataStore(FeatureStore &featureStoreIn);
+
+	void NumLayers(int numLayers);
+	void LayerStart(const char *name, int version);
+	void LayerEnd();
+
+	void Feature(int typeEnum, bool hasId, unsigned long long id, 
+		const std::map<std::string, std::string> &tagMap,
+		std::vector<Point2D> &pointsOut, 
+		std::vector<std::vector<Point2D> > &linesOut,
+		std::vector<Polygon2D> &polygonsOut);
+};
 
 #endif //_READ_INPUT_MBTILES_H
 
